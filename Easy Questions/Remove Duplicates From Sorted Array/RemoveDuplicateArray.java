cb2be0cd04dbed065693b3f57f2b2f61ef76f0e7
mickeysrishti <Mukeshpathak345@gmail.com>
2025-01-13 13:23:05 +0530

    code added

--- Easy Questions/Remove Duplicates From Sorted Array/RemoveDuplicateArray.java
+++ Easy Questions/Remove Duplicates From Sorted Array/RemoveDuplicateArray.java
@@ -13,7 +13,7 @@ public class RemoveDuplicateArray {
         
     }
     public static void main(String[] args){
-        int[] A = {0,1,1,1,2,2,3};
+        int[] A = {0,1,1,1,2,2,3,5,5};
         System.out.println(removeDuplicate(A));
 
     }
